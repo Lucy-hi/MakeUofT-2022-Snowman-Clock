4d3125a3b585d80518e4e15913a517f2709a7dde
Zicong Leo Qu <69138095+zicongleoqu@users.noreply.github.com>
2022-02-20 01:37:09 -0500

    Add files via upload

---
+++ countdown_with_button.ino
@@ -0,0 +1,80 @@
+// include the library code:
+#include <LiquidCrystal.h>
+#include <Wire.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int switchState;
+int prevSwitchState = 1;
+int counting = 0;
+int cd = 30;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.print("Countdown: ");
+  pinMode(8, INPUT);
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  
+  switchState = digitalRead(8);
+  
+  if((prevSwitchState == 0) && (switchState == 1)){
+    if(counting == 0){
+      counting = 1;
+    }else{
+      counting = 0;
+    }
+  }
+  
+  prevSwitchState = switchState;
+  
+  if (cd >= 0) {
+  
+    if (cd / 3600 >= 10) {
+      lcd.print(cd / 3600);
+    }else if (cd / 3600 > 0){
+      lcd.print("0");
+      lcd.print(cd / 3600);
+    }else {
+      lcd.print("00");
+    }
+  
+    lcd.print(":");
+  
+    if ((cd - (cd / 3600) * 3600) / 60 >= 10) {
+      lcd.print((cd - (cd / 3600) * 3600) / 60);
+    }else if ((cd - (cd / 3600) * 3600) / 60 > 0){
+      lcd.print("0");
+      lcd.print((cd - (cd / 3600) * 3600) / 60);
+    }else {
+      lcd.print("00");
+    }
+  
+    lcd.print(":");
+  
+    if (cd - (cd / 3600) * 3600 - ((cd - cd / 3600 * 3600) / 60) * 60 >= 10) {
+      lcd.print(cd - (cd / 3600) * 3600 - ((cd - cd / 3600 * 3600) / 60) * 60);
+    }else if (cd - (cd / 3600) * 3600 - ((cd - cd / 3600 * 3600) / 60) * 60 > 0) {
+      lcd.print("0");
+      lcd.print(cd - (cd / 3600) * 3600 - ((cd - cd / 3600 * 3600) / 60) * 60);
+    }else {
+      lcd.print("00");
+    }
+    
+    if (counting == 1) {
+      cd = cd - 1;
+    }
+  
+    if (cd == -1) {
+      lcd.setCursor(0, 1);
+      lcd.print("Time's Up");
+    }
+  
+    delay(1000);
+  }
+}
